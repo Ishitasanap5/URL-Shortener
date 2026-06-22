62fae981277e3c823105e8c30e1938f3470d5eae
Ishita <ishitasanap5@gmail.com>
2026-06-22 16:12:39 +0530

    refactor: introduce core beans config, externalize baseUrl, fix dependencies, and add bloom filter preload

--- src/main/java/com/ishita/urlshortener/util/BloomFilter.java
+++ src/main/java/com/ishita/urlshortener/util/BloomFilter.java
@@ -1,11 +1,15 @@
 package com.ishita.urlshortener.util;
 
 import java.util.BitSet;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class BloomFilter {
 
     private final BitSet bitSet;
     private final int size;
+
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
 
     public BloomFilter(int size) {
         this.size = size;
@@ -13,38 +17,40 @@ public class BloomFilter {
     }
 
 
-    private int hash1(String value) {
-        return mix(value.hashCode());
-    }
+    // ADD (WRITE LOCK)
+    public void add(String value) {
+        lock.writeLock().lock();
+        try {
+            int hash = value.hashCode();
+            int index1 = Math.abs(hash % size);
+            int index2 = Math.abs((hash * 31) % size);
+            int index3 = Math.abs((hash * 17) % size);
 
-    private int hash2(String value) {
-        return mix(value.hashCode() ^ (value.hashCode() >>> 16));
-    }
+            bitSet.set(index1);
+            bitSet.set(index2);
+            bitSet.set(index3);
 
-    private int mix(int hash) {
-        return (hash & 0x7fffffff) % size;
-    }
-
-    public synchronized void add(String value) {
-
-        int h1 = hash1(value);
-        int h2 = hash2(value);
-        int h3 = (h1 + h2) % size;
-
-        bitSet.set(h1);
-        bitSet.set(h2);
-        bitSet.set(h3);
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
 
+    // CHECK (READ LOCK)
     public boolean mightContain(String value) {
+        lock.readLock().lock();
+        try {
+            int hash = value.hashCode();
+            int index1 = Math.abs(hash % size);
+            int index2 = Math.abs((hash * 31) % size);
+            int index3 = Math.abs((hash * 17) % size);
 
-        int h1 = hash1(value);
-        int h2 = hash2(value);
-        int h3 = (h1 + h2) % size;
+            return bitSet.get(index1)
+                    && bitSet.get(index2)
+                    && bitSet.get(index3);
 
-        return bitSet.get(h1)
-                && bitSet.get(h2)
-                && bitSet.get(h3);
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 }
